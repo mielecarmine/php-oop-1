bde16f44af1688bb3e58cbbaf721cd535fd3a8a0
mielecarmine <mielecarmine993@gmail.com>
2024-03-04 16:38:52 +0100

    feat: print films on page

--- index.php
+++ index.php
@@ -9,9 +9,29 @@ require_once __DIR__ . './db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Film DB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="container">
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Language</th>
+                    <th scope="col">Rating</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($films as $film): ?>
+                <tr>
+                    <td><?= $film->title?></td>
+                    <td><?= $film->lang?></td>
+                    <td><?= $film->vote?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
