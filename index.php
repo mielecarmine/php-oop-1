87540b0020bc84080d6c76ac1bc71c2596bc531c
mielecarmine <mielecarmine993@gmail.com>
2024-03-04 15:32:47 +0100

    add: constructor and methods in Production class

--- index.php
+++ index.php
@@ -11,8 +11,44 @@ class Production {
         $this->lang = $lang;
         $this->vote = $vote;
     }
+
+    function getVote() {
+        return $this->vote;
+    }
+
+    function setVote($user_vote) {
+        $this->vote = $user_vote;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function setTitle($user_title) {
+        $this->vote = $user_title;
+    }
+
+    function getLang() {
+        return $this->lang;
+    }
+
+    function setLang($user_lang) {
+        $this->vote = $user_lang;
+    }
 }
 
 $dune = new Production("Dune", "eng", 8.5);
 $star_wars = new Production("Star Wars", "eng", 6.4);
-var_dump($film_1, $film_2);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
